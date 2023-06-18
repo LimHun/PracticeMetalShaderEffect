8534f823d1a75a4608675bae3e9853933ac8f986
LimHun <dlagnskkld3@gmail.com>
2023-06-18 21:34:11 +0900

    Initial Commit

---
+++ PracticeMetalShaderEffect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeMetalShaderEffect
+//
+//  Created by 임훈 on 2023/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeMetalShaderEffect/PracticeMetalShaderEffectApp.swift
@@ -0,0 +1,18 @@
+//
+//  PracticeMetalShaderEffectApp.swift
+//  PracticeMetalShaderEffect
+//
+//  Created by 임훈 on 2023/06/18.
+//
+
+import SwiftUI
+
+@main
+struct PracticeMetalShaderEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            // 코드 추가
+        }
+    }
+}
